b0fe8e6b826defc370812272abc4cf09dfe11108
mjolsic <wongqianjie1234@gmail.com>
2023-01-01 17:50:34 +1100

    Revert "Updated QuestAcceptCondition"
    This reverts commit 604398a61a5e9526de09e142e6cf8671808717d6.

--- src/main/java/emu/grasscutter/game/quest/conditions/ConditionStateNotEqual.java
+++ src/main/java/emu/grasscutter/game/quest/conditions/ConditionStateNotEqual.java
@@ -13,9 +13,15 @@ public class ConditionStateNotEqual extends BaseCondition {
     @Override
     public boolean execute(Player owner, QuestData questData, QuestData.QuestAcceptCondition condition, String paramStr, int... params) {
         GameQuest checkQuest = owner.getQuestManager().getQuestById(condition.getParam()[0]);
-        int questState = checkQuest == null ? 0 : checkQuest.getState().getValue();
-        
-        return questState != condition.getParam()[1];
+        if (checkQuest == null) {
+            /*
+            Will spam the console
+            //Grasscutter.getLogger().debug("Warning: quest {} hasn't been started yet!", condition.getParam()[0]);
+
+            */
+            return false;
+        }
+        return checkQuest.getState().getValue() != condition.getParam()[1];
     }
 
 }
