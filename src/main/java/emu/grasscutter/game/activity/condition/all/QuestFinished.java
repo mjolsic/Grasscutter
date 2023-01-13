2053aa97c6ee476bc2c3a4cb32404c6c32e94404
WQJ <wongqianjie1234@gmail.com>
2023-01-13 23:13:25 +1100

    Fix bugs with activity quest finish condition

--- src/main/java/emu/grasscutter/game/activity/condition/all/QuestFinished.java
+++ src/main/java/emu/grasscutter/game/activity/condition/all/QuestFinished.java
@@ -4,20 +4,20 @@ import emu.grasscutter.game.activity.ActivityConfigItem;
 import emu.grasscutter.game.activity.PlayerActivityData;
 import emu.grasscutter.game.activity.condition.ActivityCondition;
 import emu.grasscutter.game.activity.condition.ActivityConditionBaseHandler;
-import emu.grasscutter.game.quest.GameQuest;
+import emu.grasscutter.game.quest.QuestManager;
 import emu.grasscutter.game.quest.enums.QuestState;
 
 import static emu.grasscutter.game.activity.condition.ActivityConditions.NEW_ACTIVITY_COND_QUEST_FINISH;
+
+import java.util.stream.IntStream;
 
 @ActivityCondition(NEW_ACTIVITY_COND_QUEST_FINISH)
 public class QuestFinished extends ActivityConditionBaseHandler {
     @Override
     public boolean execute(PlayerActivityData activityData, ActivityConfigItem activityConfig, int... params) {
-        GameQuest quest = activityData
-            .getPlayer()
-            .getQuestManager()
-            .getQuestById(params[0]);
-
-        return quest != null && quest.getState() == QuestState.FINISHED;
+        QuestManager questManager = activityData.getPlayer().getQuestManager();
+        // can have multiple parameters to check
+        return IntStream.of(params).allMatch(q -> questManager.getQuestById(q) != null
+            && questManager.getQuestById(q).getState() == QuestState.QUEST_STATE_FINISHED);
     }
 }
