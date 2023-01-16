8f4174c66f8828a9c953bd1ea51ff9148fe7bb13
WQJ <wongqianjie1234@gmail.com>
2023-01-16 21:59:40 +1100

    Updated talk related quest content

--- src/main/java/emu/grasscutter/game/quest/content/ContentCompleteAnyTalk.java
+++ src/main/java/emu/grasscutter/game/quest/content/ContentCompleteAnyTalk.java
@@ -5,7 +5,7 @@ import emu.grasscutter.game.quest.GameQuest;
 import emu.grasscutter.game.quest.QuestValueContent;
 import lombok.val;
 
-import java.util.Arrays;
+import java.util.Stream;
 
 import static emu.grasscutter.game.quest.enums.QuestContent.QUEST_CONTENT_COMPLETE_ANY_TALK;
 
@@ -14,19 +14,8 @@ public class ContentCompleteAnyTalk extends BaseContent {
 
     @Override
     public boolean execute(GameQuest quest, QuestData.QuestContentCondition condition, String paramStr, int... params) {
-        val talkId = params[0];
-        val conditionTalk = Arrays.stream(condition.getParamStr().split(","))
-            .mapToInt(Integer::parseInt)
-            .toArray();
-        return Arrays.stream(conditionTalk).anyMatch(tids -> tids == talkId)
-            || Arrays.stream(conditionTalk).anyMatch(tids -> {
-            val checkMainQuest = quest.getOwner().getQuestManager().getMainQuestByTalkId(tids);
-            if (checkMainQuest == null) {
-                return false;
-            }
-            val talkData = checkMainQuest.getTalks().get(talkId);
-            return talkData != null;
-        });
+        return Stream.of(condition.getParamStr().split(",")).mapToInt(Integer::parseInt).anyMatch(talkId -> 
+            GameData.getTalkConfigDataMap().get(params[0]) != null && talkId == params[0]);
     }
 
 }
--- src/main/java/emu/grasscutter/game/quest/content/ContentCompleteTalk.java
+++ src/main/java/emu/grasscutter/game/quest/content/ContentCompleteTalk.java
@@ -1,5 +1,7 @@
 package emu.grasscutter.game.quest.content;
 
+import emu.grasscutter.data.GameData;
+import emu.grasscutter.data.excels.QuestData;
 import emu.grasscutter.data.excels.QuestData;
 import emu.grasscutter.game.quest.GameQuest;
 import emu.grasscutter.game.quest.QuestValueContent;
@@ -12,13 +14,6 @@ public class ContentCompleteTalk extends BaseContent {
 
     @Override
     public boolean execute(GameQuest quest, QuestData.QuestContentCondition condition, String paramStr, int... params) {
-        val talkId = condition.getParam()[0];
-        val checkMainQuest = quest.getOwner().getQuestManager().getMainQuestByTalkId(talkId);
-        if (checkMainQuest == null) {
-            return false;
-        }
-
-        val talkData = checkMainQuest.getTalks().get(talkId);
-        return talkData != null;
+        return condition.getParam()[0] == params[0] && GameData.getTalkConfigDataMap().get(condition.getParam()[0]) != null;
     }
 }
